4e9f5048dfdd918bf97a0a3816b50fe824dbf646
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2021-01-19 05:25:04 -0600

    using logger and disabling schedules

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const options = require('./settings/options.js');
 
 var log4js = require('log4js');
 var logger = log4js.getLogger();
+logger.level = 'debug';
 
 const express = require('express');
 const app = express();
-
 
 require('./routes.js')(app, logger);
 
--- services/hue.js
+++ services/hue.js
@@ -3,14 +3,35 @@ var v3 = require('node-hue-api').v3,
 host = options.hueBridgeIp,
 username = options.hueUser;
 
-var motionSensorTimeout = null;
+var motionSensorTimeout = null,
+	scheduleTimeout = null;
 
 module.exports = function(logger) {
 
 	var api = null;
     (async function() {
         api = await v3.api.createLocal(host).connect(username);
+		await temporarilyDisableSchedule(2)
     })();
+
+
+	async function temporarilyDisableSchedule(hours){
+		
+		  await toggleSchedule(false);
+		  clearTimeout(scheduleTimeout);
+		  scheduleTimeout = setTimeout(async ()=>{
+			  await toggleSchedule(true);
+		  }, hours * 60 * 60 * 1000);
+	}
+
+	async function toggleSchedule(enable){
+		const mySchedule = await api.schedules.getSchedule(2);
+		mySchedule.status = enable ? 'enabled' : 'disabled';
+		api.schedules.updateSchedule(mySchedule)
+		  .then(updateResult => {
+			logger.debug(`Updated Schedule ${JSON.stringify(updateResult)}`); 
+		  });
+	}
 
 	async function temporarilyDisableMotionSensorFor(hours){
 		await toggleSensor(false);
@@ -29,7 +50,7 @@ module.exports = function(logger) {
 
 			api.sensors.updateSensorConfig(sensor)
 			.then(result => {
-				console.log(`Updated sensor config? ${result}`);
+				logger.debug(`Updated sensor config? ${result}`);
 			})
         });
 
