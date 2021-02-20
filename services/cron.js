c1d553561b4ca3e758330e134ca76070466a7ae6
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2021-02-20 05:38:39 -0600

    keeping track of last sensor data to compare week to week

--- services/cron.js
+++ services/cron.js
@@ -1,5 +1,6 @@
 const options = require('../settings/options.js');
 var CronJob = require('cron').CronJob;
+var lastSensor = null;
 
 module.exports = async function(logger,hue) {
 	const messenger = require('./messenger.js')(logger);
@@ -8,12 +9,19 @@ module.exports = async function(logger,hue) {
 		var sensorData = await hue.getSensorData();
 		var sendBatteryLowEmail = false;
 		var sensorInfo = "";
+		var lastSensorInfo = "";
 		sensorData.forEach(sensor => {
 			if(sensor.battery < options.batteryPercentWarning){
 				sensorInfo += `${sensor.name} is at ${sensor.battery}%\n`;
 				sendBatteryLowEmail = true;
 			}
 		});
+		
+		// if (sendBatteryLowEmail){
+		// 	lastSensor = sensorData;
+		// 	sendBatteryLowEmail += `${sensor.name} is at ${sensor.battery}%\n`;
+		// }
+
 		if(sendBatteryLowEmail){
 			messenger.sendEmail('Battery low', sensorInfo);
 			logger.debug('Battery low sending email:', sensorInfo);
